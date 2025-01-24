2b5afbe1c2389d3b694a44534cb16c0f350f912c
Noah Davis <11672+noahd1@users.noreply.github.com>
2025-01-23 21:41:44 -0800

    Remove spec

--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Sample do
   it "can add two numbers" do
     expect(Sample.add(3,4)).to eq(7)
   end
-
-  it "can add subtract numbers" do
-    expect(Sample.subtract(5,2)).to eq(3)
-  end
 end
